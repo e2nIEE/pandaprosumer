a8f0fc2c6014b3b2b9f15c55cc153129e199c5ec
mena138 <hyacinthe.menard@eifer.org>
2025-07-20 21:28:02 +0200

    revert fluid mix order as index

--- src/pandaprosumer/mapping/fluid_mix.py
+++ src/pandaprosumer/mapping/fluid_mix.py
@@ -41,7 +41,6 @@ class FluidMixMapping(BaseMapping):
     def __str__(self):
         return "FluidMixMapping"
 
-
     @property
     def name(self):
         return "FluidMixMapping"
@@ -65,8 +64,8 @@ class FluidMixMapping(BaseMapping):
         # FixMe: Will break if the order are not 0, 1, 2, ...
         if self._check_order(): self.check_controllers_orders(initiator_controller, responder_controller)
 
-        order = self.responder_net.mapping.at[self.index, 'order']
-        initiator_mapped_results = initiator_controller.result_mass_flow_with_temp[order]
+        # order = self.responder_net.mapping.at[self.index, 'order']
+        initiator_mapped_results = initiator_controller.result_mass_flow_with_temp[self.order]
         initiator_temperature = initiator_mapped_results[self.TEMPERATURE_KEY]
         initiator_mass_flow = initiator_mapped_results[self.MASS_FLOW_KEY]
 
