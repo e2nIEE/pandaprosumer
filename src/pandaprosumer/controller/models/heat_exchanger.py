3913f5674f958b4a20d5d20e8a940c38225d0f5c
mena138 <hyacinthe.menard@eifer.org>
2025-07-29 14:45:10 +0200

    hx case t_1_in_c >= t_out_2_required_c

--- src/pandaprosumer/controller/models/heat_exchanger.py
+++ src/pandaprosumer/controller/models/heat_exchanger.py
@@ -291,7 +291,7 @@ class HeatExchangerController(BasicProsumerController):
             # FixMe: What to do in these cases ?
             t_out_2_required_c_init = t_out_2_required_c
             if t_1_in_c < t_out_2_required_c:
-                t_out_2_required_c = t_1_in_c - self._get_element_param(prosumer, 'delta_t_hot_default_c')
+                t_out_2_required_c = t_1_in_c - min(self._get_element_param(prosumer, 'delta_t_hot_default_c'), 5)  # FixMe: case where t_out_2_required_c == t_in_2_required_c
             if t_out_2_required_c < t_in_2_required_c:
                 t_in_2_required_c += t_out_2_required_c - t_out_2_required_c_init
 
