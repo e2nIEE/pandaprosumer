d9b9d8f3cc50a4cc204f7bfba58d20750e4c8b61
critzenhoff <carl.ritzenhoff@uni-kassel.de>
2025-11-17 15:51:50 +0100

    rerun = False refixed after merging conflict

--- src/pandaprosumer/run_time_series.py
+++ src/pandaprosumer/run_time_series.py
@@ -39,6 +39,8 @@ def run_loop(net, ts_variables, run_control_fct=run_control, output_writer_fct=_
         print_progress(i, time_step, ts_variables["time_steps"], ts_variables["verbose"], ts_variables=ts_variables, **kwargs)
         if "transient" in kwargs:
             kwargs["simulation_time_step"] = i
+
+        net.rerun = False
         run_time_step(net, time_step, ts_variables, run_control_fct, output_writer_fct, **kwargs)
 
         if check_results_fct is not None:
